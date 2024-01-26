186d996f4853fe46007d1d0237919dfccfe04ab1
Himanshu <himanshuparodiya@gmail.com>
2024-01-26 22:33:56 +0530

    Create first ts file

---
+++ Basics/sample.js
@@ -0,0 +1,6 @@
+var a = "Hello";
+console.log(a);
+var b = 23;
+console.log(b);
+var c = false;
+console.log(c);
---
+++ Basics/sample.ts
@@ -0,0 +1,8 @@
+const a = "Hello"
+console.log(a);
+
+const b = 23;
+console.log(b);
+
+const c = false;
+console.log(c);
