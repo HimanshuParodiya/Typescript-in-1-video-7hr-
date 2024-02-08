2cb39507f11167fd9da8c43117bb4526b437e555
Himanshu <himanshuparodiya@gmail.com>
2024-02-08 12:58:56 +0530

    useState hook with typescript

--- 13 React TypeScript Basics/src/App.tsx
+++ 13 React TypeScript Basics/src/App.tsx
@@ -1,40 +1,82 @@
-import { useState } from "react";
-import Box from "./components/Box";
+// import { useState } from "react";
+// import Box from "./components/Box";
 
-function App() {
-  const [val, setVal] = useState<string>("");
+// function App() {
+//   const [val, setVal] = useState<string>("");
 
+//   return (
+//     <>
+//       <div>
+//         {/* <Box
+//           // heading={23} //Type 'number' is not assignable to type 'string'
+//           heading="This is box heading"
+//           count={1}
+//           func={() => {
+//             alert("Function as a props");
+//           }}
+//           children={<div>This is children as a props</div>}
+//         /> */}
+
+//         {/* // children can also be pass as  */}
+//         {/* <Box
+//           // heading={23} //Type 'number' is not assignable to type 'string'
+//           heading="This is box heading"
+//           count={1}
+//           func={() => {
+//             alert("Function as a props");
+//           }}
+//         >
+//           {"This is also an example of passing children"}
+//         </Box> */}
+
+//         {/* // Generic  */}
+
+//         <Box label="Search" value={val} setter={setVal} />
+//       </div>
+//     </>
+//   );
+// }
+
+// export default App;
+
+// Hook with interface
+import { FormEvent, useState } from "react";
+
+interface Person {
+  name: string;
+  age: number;
+}
+
+const App = () => {
+  const [user, setUser] = useState<Person>();
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(user);
+  };
   return (
     <>
-      <div>
-        {/* <Box
-          // heading={23} //Type 'number' is not assignable to type 'string'
-          heading="This is box heading"
-          count={1}
-          func={() => {
-            alert("Function as a props");
-          }}
-          children={<div>This is children as a props</div>}
-        /> */}
+      <form onSubmit={submitHandler}>
+        <input
+          type="number"
+          placeholder="Age"
+          value={user?.age || ""}
+          onChange={(e) =>
+            setUser((prev) => ({ ...prev!, age: Number(e.target.value) }))
+          }
+        />
+        <input
+          type="Text"
+          placeholder="Name"
+          value={user?.name || ""} // getting error of ( A component is changing an uncontrolled input to be controlled.) because user.name is initially undefined so we pass || "" to make it controlled form uncontrolled
+          onChange={(e) =>
+            setUser((prev) => ({ ...prev!, name: e.target.value }))
+          }
+        />
 
-        {/* // children can also be pass as  */}
-        {/* <Box
-          // heading={23} //Type 'number' is not assignable to type 'string'
-          heading="This is box heading"
-          count={1}
-          func={() => {
-            alert("Function as a props");
-          }}
-        >
-          {"This is also an example of passing children"}
-        </Box> */}
-
-        {/* // Generic  */}
-
-        <Box label="Search" value={val} setter={setVal} />
-      </div>
+        <button type="submit">Register</button>
+      </form>
     </>
   );
-}
+};
 
 export default App;
