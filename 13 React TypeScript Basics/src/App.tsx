d0911dbeefcbe4371caafc0819cefea8e2f9a185
Himanshu <himanshuparodiya@gmail.com>
2024-02-08 13:34:35 +0530

    TypeScript with useContext

--- 13 React TypeScript Basics/src/App.tsx
+++ 13 React TypeScript Basics/src/App.tsx
@@ -39,43 +39,82 @@
 
 // export default App;
 
-// Hook with interface
-import { FormEvent, useState } from "react";
+// // Hook with interface
+// import { FormEvent, useState } from "react";
 
-interface Person {
-  name: string;
-  age: number;
+// interface Person {
+//   name: string;
+//   age: number;
+// }
+
+// const App = () => {
+//   const [user, setUser] = useState<Person>();
+//   const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+//     e.preventDefault();
+//     console.log(user);
+//   };
+//   return (
+//     <>
+//       <form onSubmit={submitHandler}>
+//         <input
+//           type="number"
+//           placeholder="Age"
+//           value={user?.age || ""}
+//           onChange={(e) =>
+//             setUser((prev) => ({ ...prev!, age: Number(e.target.value) }))
+//           }
+//         />
+//         <input
+//           type="Text"
+//           placeholder="Name"
+//           value={user?.name || ""} // getting error of ( A component is changing an uncontrolled input to be controlled.) because user.name is initially undefined so we pass || "" to make it controlled form uncontrolled
+//           onChange={(e) =>
+//             setUser((prev) => ({ ...prev!, name: e.target.value }))
+//           }
+//         />
+
+//         <button type="submit">Register</button>
+//       </form>
+//     </>
+//   );
+// };
+
+// export default App;
+
+// use Context hook with typescript
+import { ReactNode, createContext, useState } from "react";
+import Box from "./components/Box";
+
+type ThemeType = "light" | "dark";
+
+interface ThemeContextType {
+  theme: ThemeType;
+  toggleTheme: () => void;
 }
 
-const App = () => {
-  const [user, setUser] = useState<Person>();
-  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    console.log(user);
+export const ThemeContext = createContext<ThemeContextType>({
+  theme: "light",
+  toggleTheme: () => {},
+});
+
+const ThemeProvider = ({ children }: { children: ReactNode }) => {
+  const [mode, setMode] = useState<ThemeType>("light");
+  const toggleTheme = () => {
+    setMode((prev) => (prev === "light" ? "dark" : "light"));
   };
   return (
-    <>
-      <form onSubmit={submitHandler}>
-        <input
-          type="number"
-          placeholder="Age"
-          value={user?.age || ""}
-          onChange={(e) =>
-            setUser((prev) => ({ ...prev!, age: Number(e.target.value) }))
-          }
-        />
-        <input
-          type="Text"
-          placeholder="Name"
-          value={user?.name || ""} // getting error of ( A component is changing an uncontrolled input to be controlled.) because user.name is initially undefined so we pass || "" to make it controlled form uncontrolled
-          onChange={(e) =>
-            setUser((prev) => ({ ...prev!, name: e.target.value }))
-          }
-        />
+    <ThemeContext.Provider value={{ theme: mode, toggleTheme: toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
 
-        <button type="submit">Register</button>
-      </form>
-    </>
+const App = () => {
+  return (
+    <ThemeProvider>
+      <div>Hello</div>
+      <Box />
+    </ThemeProvider>
   );
 };
 
--- 13 React TypeScript Basics/src/components/Box.tsx
+++ 13 React TypeScript Basics/src/components/Box.tsx
@@ -1,31 +1,49 @@
-import { Dispatch, SetStateAction } from "react";
+// import { Dispatch, SetStateAction } from "react";
 
-type InputValType = string | number;
+import { useContext } from "react";
+import { ThemeContext } from "../App";
 
-type PropsType<T> = {
-  label: string;
-  value: T; // Cannot find name 'T' so we pass <T> after PropsType
-  setter: Dispatch<SetStateAction<T>>;
-};
+// type InputValType = string | number;
 
-const Box = <T extends InputValType>({
-  label,
-  value,
-  setter,
-}: PropsType<T>) => {
-  // Extend the T For InputValType (because T is not assignable to value) so now value can be string or number
+// type PropsType<T> = {
+//   label: string;
+//   value: T; // Cannot find name 'T' so we pass <T> after PropsType
+//   setter: Dispatch<SetStateAction<T>>;
+// };
+
+// const Box = <T extends InputValType>({
+//   label,
+//   value,
+//   setter,
+// }: PropsType<T>) => {
+//   // Extend the T For InputValType (because T is not assignable to value) so now value can be string or number
+//   return (
+//     <form>
+//       <label>{label}</label>
+//       <input
+//         type="text"
+//         value={value}
+//         onChange={(e) => setter(e.target.value as T)}
+//       />
+//     </form>
+//   );
+// };
+
+// export default Box;
+
+// // <T,> declare generic
+
+const Box = () => {
+  const { theme, toggleTheme } = useContext(ThemeContext);
+  //   alert(theme);
+  console.log(theme);
+
   return (
-    <form>
-      <label>{label}</label>
-      <input
-        type="text"
-        value={value}
-        onChange={(e) => setter(e.target.value as T)}
-      />
-    </form>
+    <div className="boxContainer">
+      <h1>Box 1</h1>
+      <button onClick={() => toggleTheme()}>Change Theme</button>
+    </div>
   );
 };
 
 export default Box;
-
-// <T,> declare generic
