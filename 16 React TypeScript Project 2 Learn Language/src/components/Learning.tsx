07e0ed0f3b1b841a26c47e9badd1efb3edd7a3d6
Himanshu <himanshuparodiya@gmail.com>
2024-02-11 11:18:06 +0530

    Finish the ui of learning page

--- 16 React TypeScript Project 2 Learn Language/src/components/Learning.tsx
+++ 16 React TypeScript Project 2 Learn Language/src/components/Learning.tsx
@@ -1,7 +1,66 @@
-import React from "react";
+import { ArrowBack, VolumeUp } from "@mui/icons-material";
+import { Button, Container, Stack, Typography } from "@mui/material";
+import { useState } from "react";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Learning = () => {
-  return <div>Learning</div>;
+  const [questionCount, setQuestionCount] = useState<number>(0);
+  const params = useSearchParams()[0].get("language") as LanguageType;
+  console.log(params); // ja for japanese
+
+  const navigate = useNavigate();
+
+  const nextHandler = (): void => {
+    setQuestionCount((prev) => prev + 1);
+  };
+
+  return (
+    <Container
+      maxWidth={"sm"}
+      sx={{
+        padding: "1rem",
+      }}
+    >
+      <Button
+        onClick={
+          questionCount === 0 // if the question number is 1
+            ? () => navigate("/") // then go back to home
+            : () => setQuestionCount((prev) => prev - 1) // if question number is 4 then go back to question number 3
+        }
+      >
+        <ArrowBack />
+      </Button>
+      <Typography>Learning Mode Easy</Typography>
+
+      <Stack direction={"row"} spacing={"1rem"}>
+        <Typography variant="h4">
+          {questionCount + 1} - {"Sample"}
+        </Typography>
+        <Typography color={"blue"} variant="h4">
+          : {"Lol"}
+        </Typography>
+        <Button
+          sx={{
+            borderRadius: "50%",
+          }}
+        >
+          <VolumeUp />
+        </Button>
+      </Stack>
+
+      <Button
+        sx={{ margin: "3rem 0" }}
+        variant="contained"
+        fullWidth
+        onClick={questionCount === 7 ? () => navigate("/quiz") : nextHandler}
+      >
+        {questionCount === 7 ? "Test" : "Next"}
+      </Button>
+    </Container>
+  );
 };
 
 export default Learning;
+
+// Use useParams when you want to extract parameters from the route path.
+// Use useSearchParams when you want to work with query parameters in the URL.
--- 16 React TypeScript Project 2 Learn Language/src/vite-env.d.ts
+++ 16 React TypeScript Project 2 Learn Language/src/vite-env.d.ts
@@ -1,1 +1,3 @@
 /// <reference types="vite/client" />
+
+type LanguageType = "ja" | "hi" | "es" | "fr";
